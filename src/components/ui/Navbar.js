797791a6593353519b0e78d883e7d057d75ba062
ruben <ruben@IMACPRO.local>
2024-02-23 19:58:56 +0100

    updated Navbar links

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,6 +1,11 @@
 import { Link, NavLink } from "react-router-dom";
 
 export const Navbar = () => {
+  const handleLogout = () => {
+    //TODO: Logout
+    console.log("Logout");
+  };
+
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
       <Link className="navbar-brand" to="/">
@@ -19,9 +24,15 @@ export const Navbar = () => {
         </div>
       </div>
 
-      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
-          <NavLink className="nav-item nav-link" to="/login">
+          <span className="nav-item nav-link text-info">Ruben</span>
+
+          <NavLink
+            onClick={handleLogout}
+            className="nav-item nav-link"
+            to="/login"
+          >
             Logout
           </NavLink>
         </ul>
