e4147c933d83d57c6bf53a8f23e7b386511e9a48
ruben <ruben@IMACPRO.local>
2024-02-25 17:08:47 +0100

    to local storage

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,14 +1,27 @@
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { types } from '../../types/types';
+import { AuthContext } from '../../auth/authContext';
 
 
 export const LoginScreen = () => {
 
     const navigate = useNavigate();
+    const {dispatch} = useContext (AuthContext)
 
     const handleLogin = () => {
-        navigate('/marvel', {
+        /* navigate('/marvel', {
             replace: true
-        });
+        }); */
+        const action = {
+            type: types.login,
+            payload: { name: 'Ruben ON' }
+        }
+       dispatch(action);
+
+       navigate('/marvel', {
+        replace: true   
+       })
     }
 
 
